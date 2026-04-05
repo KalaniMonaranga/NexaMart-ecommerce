34cce7ebfad38e4a2fb660e43ed16fcfab483fa3
Kalani Monaranga <kalanimonaranga@gmail.com>
2026-04-06 03:26:18 +0530

    feat: integrate global footer component into main app layout

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Footer from "./components/Footer"; 
+import Footer from "./src/components/Footer";
 
 // 1. Context Provider
-import { CartProvider } from './src/context/CartContext.jsx'; 
-import { AuthProvider } from './src/context/AuthContext.jsx'; 
-import { WishlistProvider } from './src/context/WishlistContext.jsx'; 
+import { CartProvider } from './src/context/CartContext.jsx';
+import { AuthProvider } from './src/context/AuthContext.jsx';
+import { WishlistProvider } from './src/context/WishlistContext.jsx';
 
 // 2. Styles
 import './src/index.css'; // Make sure your global styles are imported here
@@ -13,8 +13,8 @@ import './src/index.css'; // Make sure your global styles are imported here
 // 3. Components & Pages
 import Navbar from './src/components/Navbar';
 import Home from './src/Pages/Home';
-import Shop from './src/Pages/Shop'; 
-import AdminPanel from './src/Pages/AdminPanel'; 
+import Shop from './src/Pages/Shop';
+import AdminPanel from './src/Pages/AdminPanel';
 import AccountPage from './src/Pages/AccountPage';
 import ProductPage from './src/Pages/ProductPage';
 import ProductDetail from './src/Pages/ProductDetail';
@@ -40,10 +40,10 @@ function App() {
           <Router>
             {/* We move the background color here to cover the whole screen */}
             <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-              
+
               {/* Pass user from AuthContext to Navbar */}
-              <Navbar /> 
-              
+              <Navbar />
+
               {/* Removed the 'container' class from main so banners can be full-width */}
               <main style={{ flex: 1 }}>
                 <Routes>
@@ -67,8 +67,8 @@ function App() {
                   <Route path="/contact" element={<ContactUs />} />
                 </Routes>
               </main>
-              
-              {/* You can add a <Footer /> here later */}
+
+              <Footer />
             </div>
           </Router>
         </WishlistProvider>
